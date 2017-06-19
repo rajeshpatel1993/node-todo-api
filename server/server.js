1fa96d4e1f61deda6e9d41d39b328848d79d5410
Rajesh <rajesh.patelp3034@gmail.com>
2017-06-19 09:40:09 +0530

    added jwt token

--- server/server.js
+++ server/server.js
@@ -100,6 +100,24 @@ app.patch('/todos/:id',(req,res)=>{
 
 });
 
+
+app.post('/users',(req,res)=>{
+
+
+
+    var body = _.pick(req.body,['email','password']);
+    var user = new User(body);
+    user.save().then(()=>{
+       return user.generateAuthToken();
+
+        //res.send(user);
+    }).then((token)=>{
+        res.header('x-auth',token).status(200).send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+
+});
 app.listen(port,()=>{
     console.log(`Starting on port ${port}`);
 });
