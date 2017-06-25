acaf9d1e291fb26b0275950ef03c4cdf6dc8d744
Rajesh <rajesh.patelp3034@gmail.com>
2017-06-25 08:13:42 +0530

    update config

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === "development"){
-    process.env.PORT = 3000;
-    process.env.PROD_MONGODB = 'mongodb://localhost:27017/todoapp';
-}else if(env === "test"){
-    process.env.PORT = 3000;
-    process.env.PROD_MONGODB = 'mongodb://localhost:27017/todoapptest';
+
+if(env === "development" || env ==="test"){
+    var config = require('./config.json');
+    var envConfig = config[env];
+  // console.log(config.development);
+
+   Object.keys(envConfig).forEach((key)=>{
+      process.env[key] = envConfig[key];
+
+   });
+
 }
+// if(env === "development"){
+//     process.env.PORT = 3000;
+//     process.env.PROD_MONGODB = 'mongodb://localhost:27017/todoapp';
+// }else if(env === "test"){
+//     process.env.PORT = 3000;
+//     process.env.PROD_MONGODB = 'mongodb://localhost:27017/todoapptest';
+// }
